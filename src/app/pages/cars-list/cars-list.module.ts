30c2ac93634cc23691beb21f9179e6080cae352d
Rubenprada <rubenprada89@outlook.com>
2023-03-23 17:04:41 +0100

    FEAT: delete cars componente, we use them on shared module

--- src/app/pages/cars-list/cars-list.module.ts
+++ src/app/pages/cars-list/cars-list.module.ts
@@ -1,21 +1,21 @@
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CarsListRoutingModule } from './cars-list-routing.module';
 import { CarsListComponent } from './cars-list.component';
-import { CarsComponent } from './components/cars/cars.component';
 import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    CarsListComponent,
-    CarsComponent
+    CarsListComponent
   ],
   imports: [
     CommonModule,
     CarsListRoutingModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ]
 })
 export class CarsListModule { }
