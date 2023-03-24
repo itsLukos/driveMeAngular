168b1f352abd5ec7d64dbb4b74bb2ced8bb24b5c
Almudena17 <113046317+Almudena17@users.noreply.github.com>
2023-03-24 17:19:57 +0100

    login-account

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public userLogged$: ReplaySubject<boolean> = new ReplaySubject<boolean>();
+
+  constructor() { 
+
+    this.userLogged$.next(false);
+  }
+
+  public login() {
+
+    this.userLogged$.next(true);
+  }
+
+  public logout() {
+    
+    this.userLogged$.next(false);
+
+  }
+}
