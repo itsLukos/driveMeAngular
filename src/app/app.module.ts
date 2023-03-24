47da8d448438c96d4962862cc74c33e0595f7f32
Almudena17 <113046317+Almudena17@users.noreply.github.com>
2023-03-24 17:25:38 +0100

    login-routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 
 
@@ -14,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    CoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
