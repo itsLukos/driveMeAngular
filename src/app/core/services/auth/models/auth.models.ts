1a0d063ef4c7ef78ed194fc7c7088d175c674e86
Almudena17 <113046317+Almudena17@users.noreply.github.com>
2023-03-25 15:58:45 +0100

    authentication

--- src/app/core/services/auth/models/auth.models.ts
+++ src/app/core/services/auth/models/auth.models.ts
@@ -1,8 +1,26 @@
+import { Cars } from "../../cars/cars.model";
+
 export interface IUser {
+    _id?: string;
+    apellidos: string;
+    telefono?: number;
     email: string;
+    role: RoleUser;
+    favoriteCars?: Cars;
     password: string;
+    createdAt?:string;
+    updatedAt?: string;
+    __v: number;
 }
 
 export interface IUserSignInResponse {
+    user: IUser;
+}
 
+export type RoleUser =
+'buyer' | 'seller';
+
+export enum AuthError {
+    GENERIC = 0,
+    AUTH = 1
 }
