512698af230be1a3ae5a904c70280b01960cad84
Luka <107578293+itsLukos@users.noreply.github.com>
2023-03-28 22:12:42 +0200

    carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarouselComponent } from '@coreui/angular';
+import { IconComponent } from '@coreui/icons-angular'
 
 
 @Component({
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  
- 
+export class HomeComponent implements OnInit {
+
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: 'https://images.pexels.com/photos/3752169/pexels-photo-3752169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Lujo',
+      subtitle: 'Disponible en nuestra web'
+    };
+    this.slides[1] = {
+      id: 1,
+      src: 'https://images.pexels.com/photos/376361/pexels-photo-376361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Comfort',
+      subtitle: 'Disponible en nuestra web'
+    }
+    this.slides[2] = {
+      id: 2,
+      src: 'https://images.pexels.com/photos/754595/pexels-photo-754595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title: 'Aventura',
+      subtitle: 'Disponible en nuestra web'
+    }
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CarouselModule } from '@coreui/angular';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
@@ -11,7 +11,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselModule
   ]
 })
 export class HomeModule { }
